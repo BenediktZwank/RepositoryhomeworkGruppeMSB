ca04d2b9f9755ca4d524701209875d7999d9775b
BenediktZwank <benezwank0211@gmail.com>
2024-11-04 12:35:38 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello fertige Brancherstellung!");
-        System.out.println("Wwww");
-//Sharon probiert nur was aus
-        //okay
-        System.out.println("SHAron der Bossssssssss");
-
-        //Digga ich muss hier gerade ausprobieren ja, deswegen schreibe ich hier etwas unnötiges
 
     }
 }
