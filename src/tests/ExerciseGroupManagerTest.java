fdaa43d065df3ceafafccbb6f1a6f36070491ef9
mikew <mike.wallitzek@gmail.com>
2024-11-06 12:25:58 +0100

    Aufg3 zwischenspeicher

--- src/tests/ExerciseGroupManagerTest.java
+++ src/tests/ExerciseGroupManagerTest.java
@@ -6,15 +6,23 @@ public class ExerciseGroupManagerTest {
 
     ExerciseGroupManagerTest exerciseGroupManagerTest = new ExerciseGroupManagerTest();
 
-    @Test
-    public void testTotalStudents0() {
-        assertEquals(0, exerciseGroupManagerTest.totalStudents());
-    }
+    //public int checkGroupCapacities(int totalStudents , int groupSize, int availableGroups );
+    // Angenommen gibt Anzahl an übrig gebliebenen Studente an, also Studenten ohne Übungsgruppe
+
+
+    @Test //Test ob das grundsätzliche Prinzip funktioniert
+    public void TestFunction() {
+
+        assertEquals(0, exerciseGroupManagerTest.checkGroupCapacaties(100, 20, 7));
+    } //Es solle 0 rauskommen, da jeder Student einen Platz bekommen sollte
+
+    @Test // Test ob es erkennt, wenn Studenten überbleiben; Überbelastung
+    public void TestOverCapacity() {
+
+        assertEquals(20, exerciseGroupManagerTest.checkGroupCapacaties(100, 20, 4));
+    } //Es sollten 20 Studenten übrig bleiben
 
     @Test
-    public void testTotalStudents1() {
-        exerciseGroupManagerTest.addStudent("John");
-        assertEquals(1, exerciseGroupManagerTest.totalStudents());
-    }
+
 
 }
