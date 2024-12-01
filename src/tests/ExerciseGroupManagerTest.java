e3388fabcb3ea77ed509dc0586732a274632239a
mikew <mike.wallitzek@gmail.com>
2024-12-01 17:26:10 +0100

    HW6 Aufg 1 angefangen

--- src/tests/ExerciseGroupManagerTest.java
+++ src/tests/ExerciseGroupManagerTest.java
@@ -1,6 +1,11 @@
 package tests;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 public class ExerciseGroupManagerTest {
 
@@ -47,5 +52,48 @@ public class ExerciseGroupManagerTest {
 
         assertEquals(100, exerciseGroupManagerTest.checkGroupCapacaties(100, 20, 0));
     } //Es sollten 100 Studenten übrig bleiben
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,  5, 10,20,  30, 1000 })
+    public void TestDifferentGroupSizes(int groupSize) {
+        int help = 100 - (groupSize * 5); // 5 Gruppen
+        if(help < 0) help = 0;
+
+        assertEquals(help, exerciseGroupManagerTest.checkGroupCapacaties(100, groupSize, 5));
+    } //Es sollten 0 Studenten übrig bleiben
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 4, 5, 7, 8 })
+    public void TestDifferentAvailableGroups(int availableGroups) {
+        int help = 100 - (20 * availableGroups); // 20 Studenten pro Gruppe
+        if(help < 0) help = 0;
+
+        assertEquals(help, exerciseGroupManagerTest.checkGroupCapacaties(100, 20, availableGroups));
+    } //Es sollten 0 Studenten übrig bleiben
+
+    @ParameterizedTest //Test wenn Totalstudents sich ändert
+    @ValueSource(ints = {0, 10,50, 99, 100,101, 200, 250,})
+    public void TestDifferentTotalStudents(int totalStudents) {
+        int help = totalStudents - (20 * 5); // 20 Studenten pro Gruppe
+        if(help < 0) help = 0;
+
+        assertEquals(help, exerciseGroupManagerTest.checkGroupCapacaties(totalStudents, 20, 5));
+    }
+
+    @ParameterizedTest //Test ob es erkennt, wenn Parameter falsch sind
+    @CsvSource({
+            "100, 20, -5",  // Gruppenanzahl negativ
+            "100, -20, 5",  // Gruppengröße negativ
+            "-100, 20, 5",  // Studentenanzahl negativ
+    })
+    public void TestNegativeParams(int totalStudents, int groupSize, int availableGroups) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            exerciseGroupManagerTest.checkGroupCapacaties(totalStudents, groupSize, availableGroups);
+        });
+    } //Es sollten eine Exception geworfen werden, da Parameter negativ sind
+
+
+
+
 
 }
