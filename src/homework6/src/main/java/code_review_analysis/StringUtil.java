e99a09bed34994aa52d1002c797f642a2080a102
BenediktZwank <benezwank0211@gmail.com>
2024-12-03 12:40:56 +0100

    Update Homework 6.2

---
+++ src/homework6/src/main/java/code_review_analysis/StringUtil.java
@@ -0,0 +1,13 @@
+package homework6.src.main.java.code_review_analysis;
+
+
+import java.util.Optional;
+
+public class StringUtil {
+
+	public static boolean isBlank(Optional<String> citeKeyOptional) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
