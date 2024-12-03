69eea64ec566a982e43f70836e2e917881a5e038
BenediktZwank <benezwank0211@gmail.com>
2024-12-03 12:18:33 +0100

    Update Homework 6.1

--- src/tests/ExerciseGroupManagerTest.java
+++ src/tests/ExerciseGroupManagerTest.java
@@ -2,27 +2,30 @@ package tests;
 import homework3.EntityManager;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
 public class ExerciseGroupManagerTest  {
 
 
 
     ExerciseGroupManagerTest exerciseGroupManagerTest = new ExerciseGroupManagerTest();
+    public int checkGroupCapacaties(int totalStudents, int groupSize, int availableGroups){
+        int help = totalStudents- (groupSize * availableGroups);
+        if(help <= 0) {
+            return  0;
+        }
+        return help;
+    }
 
-    //public int checkGroupCapacities(int totalStudents , int groupSize, int availableGroups );
-    // Angenommen gibt Anzahl an übrig gebliebenen Studente an, also Studenten ohne Übungsgruppe
 
-
-    @Test //Test ob das grundsätzliche Prinzip funktioniert
-    public void TestFunction() {
-
-        assertEquals(0, exerciseGroupManagerTest.checkGroupCapacaties(100, 20, 5));
-        assertNotEquals(1, exerciseGroupManagerTest.checkGroupCapacaties(100, 20, 5));
-    } //Es solle 0 rauskommen, da jeder Student einen Platz bekommen sollte
-    //public int checkGroupCapacities(int totalStudents , int groupSize, int availableGroups );
-    // Angenommen gibt Anzahl an übrig gebliebenen Studente an, also Studenten ohne Übungsgruppe
-
-    // Homework 6 Tests nachfolgend:
+//Homework 6.1 nachfolgend
 
     @ParameterizedTest
     @ValueSource(ints = {0,  5, 10,20,  30, 1000 })
@@ -31,7 +34,7 @@ public class ExerciseGroupManagerTest  {
         if(help < 0) help = 0;
 
         assertEquals(help, exerciseGroupManagerTest.checkGroupCapacaties(100, groupSize, 5));
-    } //Es sollten 0 Studenten übrig bleiben
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 4, 5, 7, 8 })
@@ -40,7 +43,7 @@ public class ExerciseGroupManagerTest  {
         if(help < 0) help = 0;
 
         assertEquals(help, exerciseGroupManagerTest.checkGroupCapacaties(100, 20, availableGroups));
-    } //Es sollten 0 Studenten übrig bleiben
+    }
 
     @ParameterizedTest //Test wenn Totalstudents sich ändert
     @ValueSource(ints = {0, 10,50, 99, 100,101, 200, 250, Integer.MAX_VALUE})
@@ -50,39 +53,6 @@ public class ExerciseGroupManagerTest  {
 
         assertEquals(help, exerciseGroupManagerTest.checkGroupCapacaties(totalStudents, 20, 5));
     }
-
-    @Test // Test ob es erkennt, wenn Studenten überbleiben; Überbelastung
-    public void TestOverCapacity() {
-
-        assertEquals(20, exerciseGroupManagerTest.checkGroupCapacaties(100, 20, 4));
-        assertNotEquals(0, exerciseGroupManagerTest.checkGroupCapacaties(100, 20, 4));
-    } //Es sollten 20 Studenten übrig bleiben
-
-    @Test //Test ob es erkennt, wenn Gruppen überbleiben; Unterbelastung
-    public void TestNoStudents() {
-
-        assertEquals(0, exerciseGroupManagerTest.checkGroupCapacaties(0, 20, 10));
-        assertNotEquals(20, exerciseGroupManagerTest.checkGroupCapacaties(0, 20, 10));
-    } //Es sollten 0 Studenten übrig bleiben
-
-    @Test //Test ob es erkennt, wenn Parameter falsch sind
-    public void TestWrongParams() {
-
-        assertThrows(IllegalArgumentException.class, () -> {
-            exerciseGroupManagerTest.checkGroupCapacaties(100, -10, 4);
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            exerciseGroupManagerTest.checkGroupCapacaties(100, 10, -4);
-        });
-    } //Es sollten eine Exception geworfen werden, da Gruppenanzahl und/oder Gruppengröße negativ ist
-
-    @Test //Test ob es erkennt, wenn Gruppenanzahl 0 ist, keine Gruppen verfüpbar
-    public void TestNoGroupsAvailable() {
-
-        assertEquals(100, exerciseGroupManagerTest.checkGroupCapacaties(100, 20, 0));
-    } //Es sollten 100 Studenten übrig bleiben
-
-
 
     @ParameterizedTest //Test ob es erkennt, wenn Parameter falsch sind
     @CsvSource({
