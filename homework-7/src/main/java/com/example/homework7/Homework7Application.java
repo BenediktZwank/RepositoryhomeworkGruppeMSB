9d7ba00c5ee15894a56ef5a99942f75bde909b7d
mikew <mike.wallitzek@gmail.com>
2024-12-07 20:57:09 +0100

    HW 7 Aufg 1-3 fertig, Aufg 4 teilweise

--- homework-7/src/main/java/com/example/homework7/Homework7Application.java
+++ homework-7/src/main/java/com/example/homework7/Homework7Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Homework7Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Homework7Application.class, args);
     }
 
